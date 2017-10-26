2a1f3d0a1fc12e7d27964049d110440b5905c184
Asjas <asjas@outlook.com>
2017-10-26 21:14:49 +0200

    Fixed npm packages and gulp worker script

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,22 @@ var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var sass = require('gulp-sass');
 
-gulp.task('style-formatting', function() {
-  gulp.src('build/*.css')
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('styles'));
-});
-
+// Run sass files to css files
 gulp.task('sass', function() {
   return gulp.src('build/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('build'));
 });
 
+// Format css stylesheets with autoprefixer
+gulp.task('style-formatting', function() {
+  gulp.src('build/*.css')
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('styles'));
+});
+
+// Watch both sass and css files and update automatically
 gulp.task('watch', function() {
-  gulp.watch('build/*.css', ['style-formatting']),
-  gulp.watch('build/*.scss', ['sass'])
+  gulp.watch('build/*.scss', ['sass']),
+  gulp.watch('build/*.css', ['style-formatting'])
 });
