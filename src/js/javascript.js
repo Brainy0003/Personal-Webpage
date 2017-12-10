1c94a6ee46711c07cc2cf05e0e6019f651e68883
Asjas <asjas@outlook.com>
2017-12-10 19:01:23 +0200

    add slick.js and jquery. also code the slick slider

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,3 +1,7 @@
+/*
+  On page load I clone the header and create a sticky header that
+  renders when you scroll past a certain point in the website.
+*/
 document.addEventListener('DOMContentLoaded', () => {
   const section = document.querySelector('section');
   const header = document.querySelector('header');
@@ -15,21 +19,35 @@ document.addEventListener('DOMContentLoaded', () => {
         : (document.documentElement || document.body.parentNode || document.body).scrollTop;
     document.querySelector('body').classList.toggle('scroll', scrollFromTop > 500);
   });
-});
 
-/*
- Parallax Scene
-*/
-const scene = document.getElementById('scene');
-const parallax = new Parallax(scene);
+  /*
+    Parallax Scene in hero banner
+  */
+  const scene = document.getElementById('scene');
+  const parallax = new Parallax(scene);
 
-/*
-  Masonry
-*/
-const grid = document.querySelector('.grid');
-const masonry = new Masonry(grid, {
-  itemSelector: '.grid-item',
-  columnWidth: 120,
-  fitWidth: true,
-  gutter: 2,
+  /*
+    Masonry Grid for image showcase
+  */
+  const grid = document.querySelector('.grid');
+  const masonry = new Masonry(grid, {
+    itemSelector: '.grid-item',
+    columnWidth: 120,
+    fitWidth: true,
+    gutter: 2,
+  });
+
+  /*
+    Slider js for library showcase
+  */
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 1500,
+    dots: true,
+    centerMode: true,
+    slidesToShow: 3,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+    nextArrow: '<button type="button" class="slick-next">Mext</button>',
+  });
 });
