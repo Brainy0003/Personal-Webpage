34f3f366b115788084d00534049802b2311f7caf
Asjas <asjas@outlook.com>
2018-01-21 21:52:40 +0200

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
+const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
 
+const nodeEnv = process.env.NODE_ENV || 'production';
+
 module.exports = {
-  entry: ['./src/js/index.js', './src/_scss/style.scss'],
+  devtool: 'source-map',
+  entry: ['babel-polyfill', './src/js/index.js', './src/_scss/style.scss'],
   output: {
     filename: 'js/index.js',
     path: path.join(__dirname, './dist/'),
@@ -15,30 +23,21 @@ module.exports = {
         use: [{ loader: 'html-loader', options: { minimize: true } }],
       },
       {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          {
-            loader: 'image-webpack-loader',
-          },
-          {
-            loader: 'url-loader',
-            options: {
-              name: 'images/[name].[ext]',
-              limit: 10000,
-            },
-          },
-          {
-            loader: 'img-loader',
-          },
-        ],
-      },
-      {
         test: /\.svg$/,
         loader: 'svg-inline-loader',
       },
       {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
+          fallback: [
+            {
+              loader: 'style-loader',
+              options: {
+                hmr: true,
+                insertAt: 'top',
+              },
+            },
+          ],
           use: [
             {
               loader: 'css-loader',
@@ -57,22 +56,36 @@ module.exports = {
       },
       {
         test: /\.js$/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env'],
+            babelrc: true,
+            sourceMaps: 'both',
           },
         },
       },
     ],
   },
   plugins: [
+    new Dotenv(process.env.NODE_ENV),
+    new CleanWebpackPlugin(['dist']),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(nodeEnv) },
+    }),
     new HtmlWebPackPlugin({
       template: './src/index.html',
       filename: './index.html',
     }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'async',
+    }),
     new ExtractTextPlugin({
       filename: 'css/main.css',
+    }),
+    new UglifyJsPlugin({
+      cache: true,
+      parallel: 4,
     }),
   ],
 };
