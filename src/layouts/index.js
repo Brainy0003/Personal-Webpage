ab563f88b03f16025da6cd36561636d3ea2d87ea
Asjas <asjas@outlook.com>
2018-05-27 20:47:35 +0200

    update main layout file

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,29 +3,17 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import normalize from 'normalize.css/normalize.css';
 
-import Header from '../components/header';
-import './index.css';
-
 const Layout = ({ children, data }) => (
   <div>
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
+        { name: 'siteUrl', content: data.site.siteMetadata.siteUrl },
         { name: 'description', content: data.site.siteMetadata.description },
         { name: 'keywords', content: data.site.siteMetadata.keywords },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    {children()}
   </div>
 );
 
@@ -40,6 +28,7 @@ export const query = graphql`
     site {
       siteMetadata {
         title
+        siteUrl
         description
         keywords
       }
