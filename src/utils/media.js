e5f5bd1bfb0dceddb48099abc4fac4c4b71368dc
Asjas <asjas@outlook.com>
2018-05-27 20:44:51 +0200

    add typography and media query utils

---
+++ src/utils/media.js
@@ -0,0 +1,20 @@
+// Media Query Util file for Styled-Components
+import { css } from 'styled-components';
+
+// Mobile Query Screen Sizes
+const sizes = {
+  medium: 1024,
+  small: 768,
+  xSmall: 576,
+};
+
+// Iterate through the sizes and create a media template
+export const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)};
+    }
+  `;
+
+  return acc;
+}, {});
---
+++ src/utils/typography.js
@@ -0,0 +1,10 @@
+import Typography from 'typography';
+
+const typography = new Typography({
+  baseFontSize: '10px',
+  baseLineHeight: 1.45,
+  headerFontFamily: ['Playfair Display'],
+  bodyFontFamily: ['Montserrat'],
+});
+
+export default typography;
